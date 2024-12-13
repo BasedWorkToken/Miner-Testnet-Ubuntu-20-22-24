6e4f571d282251c7c621f77cd3855a04631bf7cf
Based Work Token <167148036+BasedWorkToken@users.noreply.github.com>
2024-12-13 15:12:47 -0600

    Update 1BasedWorkToken.sh

--- BWORK Miner Testnet Ubuntu 20&22&24 v5.1.1/1BasedWorkToken.sh
+++ BWORK Miner Testnet Ubuntu 20&22&24 v5.1.1/1BasedWorkToken.sh
@@ -4,7 +4,7 @@
 #Check for dotnet
 if ! command -v dotnet &> /dev/null
 then
-    echo "dotnet 5.0 is not found or not installed."
+    echo "dotnet 6.0 is not found or not installed."
     echo "Installing dotnet 5.0..."
 
     # Add Microsoft package signing key and repository
@@ -13,16 +13,16 @@ then
     sudo apt-get update; \
     sudo apt-get install -y apt-transport-https && \
     sudo apt-get update && \
-    sudo apt-get install -y dotnet-sdk-5.0
+    sudo apt-get install -y dotnet-sdk-6.0
 
     # Verify the installation
     dotnet --version
 
-    echo "dotnet 5.0 is installed. Rerun the script to start."
+    echo "dotnet 6.0 is installed. Rerun the script to start."
 
     goto end
 else
-    echo "dotnet 5.0 is already installed."
+    echo "dotnet 6.0 is already installed."
 fi
 
 while : ; do
